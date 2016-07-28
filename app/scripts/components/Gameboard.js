61dc6978290f9f4819f183a80fcd62d525b61f23
Shannon Riester <shannonriester@gmail.com>
2016-07-28 14:13:56 -0500

    functional ajax requests for 6 categories

--- app/scripts/components/Gameboard.js
+++ app/scripts/components/Gameboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Backbone from 'backbone';
 import $ from 'jquery';
 
 import Category from './Category';
@@ -12,57 +13,47 @@ const Gamebaord = React.createClass({
   },
   componentWillMount: function(){},
   componentDidMount: function(){
-    // this.gameCategories();
-    store.categories.fetch({
-      success: (model, response) => {
-        for (var i=0; i < 5; i++) {
-          var cluesArr = response.clues[i];
-          console.log(cluesArr);
-          this.setState({categories:store.categories.toJSON()})
-        };
-      }
+    for (i=0; i<7; i++) {
+      store.categories.add({});
+    }
+    store.categories.each(function(categoryModel){
+      categoryModel.getCategory(Math.floor(Math.random() * 18000));
     });
-    store.categories.on('change update', () => {
-      this.setState({categories:store.categories.toJSON()})
-    });
+    console.log(store.categories);
   },
-  gameCategories: function(){
-        //don't forget to change the id value in the url
+  // gameCategories: function(){
     // for (var i = 1; i <= 6; i++) {
-      // $.ajax({
-      //   type: 'GET',
-      //   url: `http://jservice.io/api/category?id=4`,
-      //   success: (response) =>{
-      //   let filterN = 200;
-      //   let category = response;
-      //   let cluesArr = category.clues.filter((clue, i, arr) => {
-      //     if (clue.value === filterN) {
-      //       filterN += 200;
-      //       return clue;
-      //     }
-      //   });
-      //   category.clues = cluesArr;
-      //   let currentState = this.state.categories;
-      //   currentState.push(category);
-      //   this.setState({categories:currentState});
-      //   }
-      // });
+    //   $.ajax({
+    //     type: 'GET',
+    //     url: `http://jservice.io/api/category?id=${id}`,
+    //     success: (response) =>{
+    //     let filterN = 200;
+    //     let category = response;
+    //     let cluesArr = category.clues.filter((clue, i, arr) => {
+    //       if (clue.value === filterN) {
+    //         filterN += 200;
+    //         return clue;
+    //       }
+    //     });
+    //     category.clues = cluesArr;
+    //     let currentState = this.state.categories;
+    //     currentState.push(category);
+    //     this.setState({categories:currentState});
+    //     }
+    //   });
     // };
-  },
+  // },
   render: function(){
-    // if (!this.state.categories){
-    //   console.log('not yet...');
-    //   return null;
-    // }
-    let categoryArr = this.state.categories.map(function(catObj, i, arr){
-      // let randomN = (Math.random());
-        //replace the numbers in the forloop with randomN
-
-        // var cluesArr = [catObj.clues[0],catObj.clues[1],catObj.clues[2],catObj.clues[3],catObj.clues[4]];
-        // console.log(cluesArr);
-        // return <h3 key={i} clues={this.state.categories.clues}></h3>;
-    });
-    // console.log(this.state.categories[0]);
+    if (!this.state.categories){
+      console.log('not yet...');
+      return null;
+    }
+    // let categoryArr = this.state.categories.map(function(catObj, i, arr){
+    //   // let randomN = (Math.random());
+    //     //replace the numbers in the forloop with randomN
+    //     return <h3 key={i} clues={this.state.categories.clues}></h3>;
+    // });
+    // console.log(categoryArr);
     return (
       <div>hi</div>
     );
--- app/scripts/models/CategoryModel.js
+++ app/scripts/models/CategoryModel.js
@@ -1,8 +1,30 @@
 import Backbone from 'backbone';
+import $ from 'jquery';
 
 const CategoryModel = Backbone.Model.extend({
   idAttribute: 'id',
   urlRoot: `http://jservice.io/api/category?id=4`,
+  defaults: {
+    category: {},
+  },
+  getCategory: function(categoryId){
+    $.ajax({
+      type: 'GET',
+      url: `http://jservice.io/api/category?id=${categoryId}`,
+      success: (response) =>{
+      let filterN = 200;
+      let category = response;
+      let cluesArr = category.clues.filter((clue, i, arr) => {
+        if (clue.value === filterN) {
+          filterN += 200;
+          return clue;
+        }
+      });
+      category.clues = cluesArr;
+      this.set('category', category);
+      }
+    });
+  },
 });
 
 export default CategoryModel;
