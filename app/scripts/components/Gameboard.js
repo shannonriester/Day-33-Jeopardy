468d6b16973c13dfdc637b8a043afcfc6f898d2e
Shannon Riester <shannonriester@gmail.com>
2016-07-28 14:27:11 -0500

    used state to allow rendering

--- app/scripts/components/Gameboard.js
+++ app/scripts/components/Gameboard.js
@@ -13,47 +13,27 @@ const Gamebaord = React.createClass({
   },
   componentWillMount: function(){},
   componentDidMount: function(){
-    for (i=0; i<7; i++) {
-      store.categories.add({});
-    }
-    store.categories.each(function(categoryModel){
+
+    store.categories.add({});
+
+    store.categories.on('update change', () => {
+      this.setState({categories: store.categories.toJSON()});
+    });
+
+    store.categories.each((categoryModel) => {
       categoryModel.getCategory(Math.floor(Math.random() * 18000));
     });
-    console.log(store.categories);
   },
-  // gameCategories: function(){
-    // for (var i = 1; i <= 6; i++) {
-    //   $.ajax({
-    //     type: 'GET',
-    //     url: `http://jservice.io/api/category?id=${id}`,
-    //     success: (response) =>{
-    //     let filterN = 200;
-    //     let category = response;
-    //     let cluesArr = category.clues.filter((clue, i, arr) => {
-    //       if (clue.value === filterN) {
-    //         filterN += 200;
-    //         return clue;
-    //       }
-    //     });
-    //     category.clues = cluesArr;
-    //     let currentState = this.state.categories;
-    //     currentState.push(category);
-    //     this.setState({categories:currentState});
-    //     }
-    //   });
-    // };
-  // },
   render: function(){
-    if (!this.state.categories){
-      console.log('not yet...');
-      return null;
-    }
-    // let categoryArr = this.state.categories.map(function(catObj, i, arr){
-    //   // let randomN = (Math.random());
-    //     //replace the numbers in the forloop with randomN
+    // if (!this.state.categories){
+    //   console.log('not yet...');
+    //   return null;
+    // }
+    console.log(store.categories.models[0].get('title'));
+    // let titles = this.state.categories.map(function(catObj, i, arr){
+    //
     //     return <h3 key={i} clues={this.state.categories.clues}></h3>;
     // });
-    // console.log(categoryArr);
     return (
       <div>hi</div>
     );
